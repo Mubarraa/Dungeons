5e7f5c1e577c111819765137756d560aa8899497
Mubarra <33150972+Mubarraa@users.noreply.github.com>
2019-12-24 16:24:33 +1100

    Add files via upload

---
+++ Entities/Sword.java
@@ -0,0 +1,15 @@
+package unsw.dungeon;
+
+public class Sword extends Entity  {
+
+	public Sword (int x, int y) {
+		super(x, y);
+	}
+	
+	@Override
+	public String toString() {
+		return "Sword";
+	}
+	
+	
+}
